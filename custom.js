ee3e003659c18b98b9f2b87c01d321e7ce9c31e5
Ömer Yaltırık <40443652+OmerYALTIRIK@users.noreply.github.com>
2022-12-05 23:58:11 +0300

    first commit

---
+++ custom.js
@@ -0,0 +1,109 @@
+const table = document.getElementById("userTable");
+
+function getUserList() {
+  fetch("https://reqres.in/api/users")
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log(data);
+      for (user of data.data) {
+        //console.log(user);
+        table.innerHTML += `<tr>
+                <td>
+                  <input type="text" class="form-control" id="first_name_${user.id}" value="${user.first_name}" />
+                </td>
+                <td>
+                  <input type="text" class="form-control" id="last_name_${user.id}" value="${user.last_name}" />
+                </td>
+                <td>
+                  <input type="text" class="form-control" id="email_${user.id}" value="${user.email}" />
+                </td>
+                <td>
+                  <a class="btn btn-warning" onclick="updateUser(${user.id})">Güncelle</a>
+                  <a class="btn btn-danger" onclick="deleteUser(${user.id})">Sil</a>
+                </td>
+              </tr>`;
+      }
+    });
+}
+
+getUserList();
+
+function refreshData() {
+  table.innerHTML = "";
+  getUserList();
+}
+
+function createUser() {
+  let data = {
+    first_name: document.getElementById("first_name").value || "Değer Yok",
+    last_name: document.getElementById("last_name").value || "Değer Yok",
+    email: document.getElementById("email").value || "Değer Yok",
+  };
+  fetch("https://reqres.in/api/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      table.innerHTML += `<tr>
+      <td>
+        <input type="text" class="form-control" id="" value="${data.first_name}" />
+      </td>
+      <td>
+        <input type="text" class="form-control" id="" value="${data.last_name}" />
+      </td>
+      <td>
+        <input type="text" class="form-control" id="" value="${data.email}" />
+      </td>
+      <td>
+        <a class="btn btn-warning" onclick="updateUser(${data.id})">Güncelle</a>
+        <a class="btn btn-danger" onclick="deleteUser(${data.id})">Sil</a>
+      </td>
+    </tr>`;
+    })
+    .catch((error) => {
+      console.log("HATA : ", error);
+    });
+}
+
+function updateUser(id) {
+  console.log(id);
+  let data = {
+    first_name:
+      document.getElementById("first_name_" + id).value || "Değer Yok",
+    last_name: document.getElementById("last_name_" + id).value || "Değer Yok",
+    email: document.getElementById("email_" + id).value || "Değer Yok",
+  };
+  console.log(data);
+  fetch("https://reqres.in/api/users", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((veri) => {
+      console.log("Kullanıcı Güncellendi : ", veri);
+    })
+    .catch((error) => console.log(error));
+}
+
+function deleteUser(id) {
+  console.log(id);
+  fetch("https://reqres.in/api/users/" + id, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => console.log(response))
+    .then((data) => {
+      console.log("Kullanıcı Silindi : ", data);
+    })
+    .catch((error) => console.log(error));
+}
